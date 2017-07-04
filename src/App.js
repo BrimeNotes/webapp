0cac887e7d87ce75ddd7fec99bd106dd8579bbd8
Ritik Aggarwal <ritik270286@gmail.com>
2017-07-05 02:55:11 +0530

    Signup and forget password page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import axios from 'axios'
 import './App.css';
+import {
+  Link,
+  BrowserRouter as Router,
+} from 'react-router-dom'
+//import {Router} from 'react-router'
+
 
 export default class App extends Component {
 
@@ -15,68 +21,124 @@ export default class App extends Component {
     this.setState({
       [e.target.name]: e.target.value,
     });
-    }
+  }
 
   onFormSubmit(e) {
     e.preventDefault();
-   console.log(this.state);
+    //console.log(this.state);
 
     axios({
-    url: this.props.action,
+      url: this.props.action,
       method: 'post',
       headers: {
         'Accept':'application/json'
-        },
+      },
       data: {
         uid: this.state.uid,
         password: this.state.password
       }
     })
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        if (error.response) {
-          console.log(error.response.data);
-          console.log(error.response.status);
-          console.log(error.response.headers);
-        }
-      });
+    .then(response => {
+      // Successful login, redirect to /upload
+      Router.push('/signin');
+
+    })
+
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      if (error.response) {
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      }
+    });
     this.setState({ uid: '', password: '' });
   }
 
-    render() {
-      return (
-        <div className="App">
-          <form
-            id="main-login"
-            action={this.props.action}
-            method={this.props.method}
-            onSubmit={this.onFormSubmit}>
-            <h2>Login Page</h2>
-            <div className="container">
-              <label><b>Username</b></label><br/>
-              <input type="text" placeholder="Enter Username" name="uid"
+
+  render() {
+    return (
+      <div className="App">
+        <form
+          id="main-login"
+          action={this.props.action}
+          method={this.props.method}
+          onSubmit={this.onFormSubmit}>
+          <h2>Login Page</h2>
+          <div className="container">
+            <label><b>Username</b></label><br/>
+            <input type="text" placeholder="Enter Username" name="uid"
+              required onChange={this.onFormChange}/><br/>
+
+              <label><b>Password</b></label><br/>
+              <input type="password" placeholder="Enter Password" name="password"
                 required onChange={this.onFormChange}/><br/>
 
-                <label><b>Password</b></label><br/>
-                <input type="password" placeholder="Enter Password" name="password"
-                  required onChange={this.onFormChange}/><br/>
+                <button type="submit">Login</button>
+              </div>
 
-                  <button type="submit">Login</button>
-                </div>
+              <div className="container" style={{backgroundColor:"#f1f1f1"}}>
+                <a href=""><span className="psw"><Link to="/reset">Forgot password?</Link></span></a><br></br>
+                New User? <a href=""><Link to="/signin">Create account</Link></a>.
+              </div>
 
-                <div className="container" style={{backgroundColor:"#f1f1f1"}}>
-                  <a href=""><span className="psw">Forgot password?</span></a><br></br>
-                  New User? <a href="">Create account.</a>
-                </div>
-
-              </form>
-            </div>
-          );
-        }
+            </form>
+          </div>
+        );
       }
+    }
+    App.defaultProps = {
+      action: 'http://brime.ml/user/login',
+    };
 
-        App.defaultProps = {
-          action: 'http://brime.ml/user/login',
-        };
+    export class Signin extends Component {
+
+      render() {
+        return (
+          <div className="App">
+            <form
+              id="main-login"
+              // action={this.props.action}
+              // method={this.props.method}
+              // onSubmit={this.onFormSubmit}
+              >
+              <h2>Signup Page</h2>
+              <div className="container">
+                <label><b>Username</b></label><br/>
+                <input type="text" placeholder="Enter Username" name="uid"
+                  required /><br/>
+
+                  <label><b>Email</b></label><br/>
+                  <input type="email" placeholder="Enter email" name="eid"
+                    required /><br/>
+
+                  <label><b>Password</b></label><br/>
+                  <input type="password" placeholder="Enter Password" name="password"
+                    required/><br/>
+
+                    <label><b>ReEnter Password</b></label><br/>
+                    <input type="password" placeholder="ReEnter Password" name="password"
+                      required/><br/>
+
+                    <button type="submit">Signup</button>
+                  </div>
+
+
+                </form>
+              </div>
+        );
+      }
+    }
+
+    export class Reset extends Component {
+
+      render() {
+        return (
+          <div>
+            <h2>Reset Page</h2>
+          </div>
+        );
+      }
+    }
--- src/index.js
+++ src/index.js
@@ -5,13 +5,17 @@ import App from './App';
 import {
   BrowserRouter as Router,
   Route,
-} from 'react-router-dom'
+} from 'react-router-dom';
+import { Reset, Signin } from './App.js';
 import registerServiceWorker from './registerServiceWorker';
+
 
 ReactDOM.render(
   <Router>
     <div>
       <Route exact path="/login" component={App}/>
+      <Route exact path="/reset" component={Reset}/>
+      <Route exact path="/signin" component={Signin}/>
     </div>
   </Router>
   , document.getElementById('root'));

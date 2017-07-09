0ef4ab28c40b912b6722e581da1587ca3ed64bce
Ritik Aggarwal <ritik270286@gmail.com>
2017-07-09 16:38:29 +0530

    Code Refactoring and Reorganisation (#6)
    * login page update
    
    * Signup and forget password page
    
    * Code Refactoring and Reorganisation

--- src/App.js
+++ src/App.js
@@ -1,82 +1,30 @@
 import React, { Component } from 'react';
-import axios from 'axios'
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
 import './App.css';
 
+import Landing from "./pages/Landing";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Reset from "./pages/Reset";
+import Footer from "./components/Footer";
+
 export default class App extends Component {
-
-  constructor(props, context) {
-    super(props, context);
-    this.state = { uid: '', password: '' };
-    this.onFormSubmit = this.onFormSubmit.bind(this);
-    this.onFormChange = this.onFormChange.bind(this);
+  render() {
+    return (
+      <div className="App">
+        <Router >
+          <div>
+            <Route exact path="/" component={Landing} />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
+            <Route path="/reset" component={Reset} />
+          </div>
+        </Router>
+        <Footer/>
+      </div>
+    );
   }
-
-  onFormChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
-    }
-
-  onFormSubmit(e) {
-    e.preventDefault();
-   console.log(this.state);
-
-    axios({
-    url: this.props.action,
-      method: 'post',
-      headers: {
-        'Accept':'application/json'
-        },
-      data: {
-        uid: this.state.uid,
-        password: this.state.password
-      }
-    })
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        if (error.response) {
-          console.log(error.response.data);
-          console.log(error.response.status);
-          console.log(error.response.headers);
-        }
-      });
-    this.setState({ uid: '', password: '' });
-  }
-
-    render() {
-      return (
-        <div className="App">
-          <form
-            id="main-login"
-            action={this.props.action}
-            method={this.props.method}
-            onSubmit={this.onFormSubmit}>
-            <h2>Login Page</h2>
-            <div className="container">
-              <label><b>Username</b></label><br/>
-              <input type="text" placeholder="Enter Username" name="uid"
-                required onChange={this.onFormChange}/><br/>
-
-                <label><b>Password</b></label><br/>
-                <input type="password" placeholder="Enter Password" name="password"
-                  required onChange={this.onFormChange}/><br/>
-
-                  <button type="submit">Login</button>
-                </div>
-
-                <div className="container" style={{backgroundColor:"#f1f1f1"}}>
-                  <a href=""><span className="psw">Forgot password?</span></a><br></br>
-                  New User? <a href="">Create account.</a>
-                </div>
-
-              </form>
-            </div>
-          );
-        }
-      }
-
-        App.defaultProps = {
-          action: 'http://brime.ml/user/login',
-        };
+}
--- src/index.js
+++ src/index.js
@@ -2,17 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {
-  BrowserRouter as Router,
-  Route,
-} from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <Router>
-    <div>
-      <Route exact path="/login" component={App}/>
-    </div>
-  </Router>
-  , document.getElementById('root'));
+  <App />  , document.getElementById('root'));
   registerServiceWorker();
